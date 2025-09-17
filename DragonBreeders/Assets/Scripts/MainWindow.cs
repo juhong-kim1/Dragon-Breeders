667f03ec87b9f4e207325876f3a25d8ef7159e0f
김주홍 <gicente1@nate.com>
2025-09-17 23:26:29 +0900

    main menu add ui

--- DragonBreeders/Assets/Scripts/MainWindow.cs
+++ DragonBreeders/Assets/Scripts/MainWindow.cs
@@ -4,16 +4,40 @@ using UnityEngine.UI;
 public class MainWindow : GenericWindow
 {
     public Button mapButton;
+    public Button menuButton;
+    public Button statButton;
+    public Button helpButton;
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject statPanel;
+    [SerializeField] private GameObject helpPanel;
 
+    private void Start()
+    {
+        menuButton.onClick.AddListener(ToggleMenu);
+        statButton.onClick.AddListener(ToggleStat);
+        helpButton.onClick.AddListener(ToggleHelp);
+        menuPanel.SetActive(false);
+        statPanel.SetActive(false);
+        helpPanel.SetActive(false);
+    }
 
     public void OnClickMap()
     {
         manager.Open(Windows.Map);
     }
 
-    public void OnClickHome()
+    private void ToggleMenu()
     {
-        manager.Open(Windows.Home);
-    
+        menuPanel.SetActive(!menuPanel.activeSelf);
+    }
+
+    private void ToggleStat()
+    {
+        statPanel.SetActive(!statPanel.activeSelf);
+    }
+
+    private void ToggleHelp()
+    {
+        helpPanel.SetActive(!helpPanel.activeSelf);
     }
 }
